7f3d60ffcf349d51ceae159c7d2ecb4e8b6d8d3f
ravindra <ravindra738738@gmail.com>
2020-12-02 11:10:02 +0530

    theory of lambda and simple program

---
+++ theoryoflambda.py
@@ -0,0 +1,11 @@
+*do not defined by 'def' keyword
+*return expression but not valus
+*single-line(one-line) function
+*Any number of arguments
+*cannot access global variable
+*Defined by using 'lambda' keyword
+*function doesn't have anyname
+
+
+sum=(lambdam x,y:x+y)
+print(sum(2,3))
